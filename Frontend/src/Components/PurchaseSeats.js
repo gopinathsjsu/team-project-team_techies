185a99d22021e820cb68d42ecdc7e2dd98507eab
nidhi-tholar <nidhi.tholarkuchur@sjsu.edu>
2021-11-29 10:46:46 -0800

    Purchase seat UI completed except for props, styling, auth

--- Frontend/src/Components/PurchaseSeats.js
+++ Frontend/src/Components/PurchaseSeats.js
@@ -4,17 +4,128 @@ import Axios from 'axios';
 import { useLocation, useHistory } from 'react-router-dom';
 
 
-// const PurchaseSeats = () => {
+const PurchaseSeats = ({}) => {
 
-//     const[seatPriceData,setseatPriceData]=useState([]);
-//     const[seatNumDictData,setseatNumData]=useState([]);
-//     const[seatTypeData,setSeatTypeData]=useState([]);
-//     const tempseatTypeData = [];
-//     const [seatNumforSeatType, setSeatNumforSeatType] = useState([]);
-//     const[selectedSeatType,setSelectedSeatType] = useState("");
-//     const[selectedSeatNum,setSelectedSeatNum] = useState("");
+    const booking = {id:'61a276afea9044c96dd4c200'}
 
-//     useEffect(()=>{
+    const[seatPriceData,setseatPriceData]=useState([]);
+    const[seatNumDictData,setseatNumData]=useState([]);
+    const[seatTypeData,setSeatTypeData]=useState([]);
+    const tempseatTypeData = [];
+    const[selectedSeatType,setSelectedSeatType] = useState("");
+    const[selectedSeatNum,setSelectedSeatNum] = useState("");
+    
+
+    useEffect(()=>{
+        const url ="http://localhost:5000/booking/purchase_seat/" +booking.id;
+        Axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+        Axios.get(url)
+        .then((response)=>{
+            console.log(response.data);
+            for (const [key, value] of Object.entries(response.data['num_of_seats'])) {
+                if (value > 0){
+                    tempseatTypeData.push(key)
+                }
+            }
+            console.log("seat type:", seatTypeData)
+            setSeatTypeData(tempseatTypeData)
+            setseatPriceData(response.data['price']);
+            setseatNumData(response.data['seat_num']);
+
+        }).catch(()=>{
+            console.log('some error occurred!')
+        })
+    },[])
+
+
+
+    const purchaseSeat = (e)=>{
+        e.preventDefault();
+        setSelectedSeatNum(e.target.value)
+        const url="http://localhost:5000/booking/purchase_seat";
+        Axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+        Axios.post( url,{
+            booking_id:booking.id,  
+            seat_type:selectedSeatType,
+            seat_num:selectedSeatNum
+      
+    }).then((response)=>{
+        console.log(response)
+    }
+
+    ).catch(()=>{
+        console.log('some error occurred!')
+        }
+    )
+    }
+
+    return (
+        <div>
+            <div className="purchase-seats-box">
+                    <h1 style={{fontWeight: "bold"}}>Purchase Seats</h1>
+            </div>
+
+            <div id="seat-type-box" className="seat-selection">
+                <h3>Seat Price</h3>
+            
+                <ul>
+                    {
+                        Object.entries(seatPriceData)
+                        .map( ([key, value]) => <li>{key}: $ {value}</li> )
+                    }
+                    
+                </ul>
+            </div>
+            
+            <div id="seat-type-box" className="seat-selection">
+                <h3>Seat Type</h3>
+            
+                {/* <select id="seatType" value={selectedSeatType} onChange={(e)=>{handleSeatTypeSelection(e);}} */}
+                <select id="seatType" value={selectedSeatType} onChange={(e)=>{setSelectedSeatType(e.target.value)}}
+                    style={{ width: "280px",height:"38px",border:"1px solid black" }}>
+
+                    <option value="" disabled selected hidden>Select Seat Type</option>
+                    {seatTypeData.map((seatType)=>{
+                        return <option value={seatType}>{seatType}</option>
+                })}
+                </select>
+            </div>
+
+            <div id="seat-type-box" className="seat-selection">
+                <h3>Seat Number</h3>
+            
+                <select id="seatNum" value={selectedSeatNum} onChange={(e)=>{purchaseSeat(e);}}
+                    style={{ width: "280px",height:"38px",border:"1px solid black" }}>
+
+                    <option value="" disabled selected hidden>Select Seat Number</option>
+                    {seatNumDictData[selectedSeatType]?.map((seatNum)=>{
+                        return <option value={seatNum}>{seatNum}</option>
+                })}
+                </select>
+            </div>
+
+            <button onClick={(e)=>{
+                                purchaseSeat(e);
+                            }}className="btn btn-primary">Purchase Seat</button> 
+
+        </div>
+        
+    )
+
+
+}
+
+
+
+// class PurchaseSeats extends Component {
+    
+//     constructor(props) {
+//         super();
+
+//         var seatTypeData = [];
+//         var seatPriceData = {};
+//         var seatNumberData = [];
+
 //         const url ="http://localhost:5000/booking/purchase_seat/61a276afea9044c96dd4c200";
 //         Axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
 //         Axios.get(url)
@@ -22,238 +133,142 @@ import { useLocation, useHistory } from 'react-router-dom';
 //             console.log(response.data);
 //             for (const [key, value] of Object.entries(response.data['num_of_seats'])) {
 //                 if (value > 0){
-//                     tempseatTypeData.push(key)
+//                     seatTypeData.push(key)
 //                 }
 //             }
-//             console.log("seat type:", seatTypeData)
-//             setSeatTypeData(tempseatTypeData)
-//             setseatPriceData(response.data['price']);
-//             setseatNumData(response.data['seat_num']);
+//             console.log(seatTypeData)
+//             seatPriceData = response.data['price']
+//             seatNumberData = response.data['seat_num']
 
 //         }).catch(()=>{
 //             console.log('some error occurred!')
 //         })
-//     },[])
 
-//     function purchaseSeat(){
-//         // call API
+//         this.state = {
+//             seatTypeData: [],
+            
+//             booking_id: '',
+//             seatTypeSelected: '',
+//             seatNumberSelected: '',
+//         }
+
+//         this.seatTypeSelectHandler = this.seatTypeSelectHandler.bind(this);
+//         this.seatNumberSelectHandler = this.seatNumberSelectHandler.bind(this);
+//         this.seatSelectSubmission = this.seatSelectSubmission.bind(this);
 //     }
 
-//     function handleSeatTypeSelection(e){
-//         console.log(e.target.value)
-//         setSelectedSeatType(e.target.value)
-//         console.log(selectedSeatType)
-//         console.log(seatNumDictData[selectedSeatType])
-//         setSeatNumforSeatType(seatNumDictData[selectedSeatType])
-//         console.log(seatNumforSeatType)
+    
+
+//     componentDidMount = () => {
+//         this.setState({
+//             seatTypeSelected: document.getElementById('seat-type-selection').value,
+//             // seatNumberSelected: document.getElementById('seat-number-selection').value
+//         });
 //     }
 
-//     return (
-//         <div>
+//     seatTypeSelectHandler = (e) => {
+//         console.log('DROPDOWN VALUE: ', e.target.value);
+//         this.setState({
+//             seatTypeSelected: e.target.value
+//         });
+//         if (e.target.value == 'window') {
+//             this.setState({
+//                 seatNumberSelected: "1A"
+//             });
+//         } else if (e.target.value == 'middle') {
+//             this.setState({
+//                 seatNumberSelected: "1B"
+//             });
+//         } if (e.target.value == 'aisle') {
+//             this.setState({
+//                 seatNumberSelected: "1C"
+//             });
+//         }
+//     }
+
+//     seatNumberSelectHandler = (e) => {
+//         console.log('DROPDOWN VALUE for seat number: ', e.target.value);
+//         this.setState({
+//             seatNumberSelected: e.target.value
+//         });
+//     }
+
+//     seatSelectSubmission = (e) => {
+//         console.log('Seat data finalized: ', this.state);
+
+//         let data = {
+//             booking_id: this.state.booking_id,
+//             seat_type: this.state.seatTypeSelected,
+//             // seat_number: this.state.seatNumberSelected
+//         }
+
+//         Axios.get('http://localhost:5000/booking/purchase_seat', data).
+//             then((response)=>{
+//             console.log('User get api response: ', response)
+//             if (response.status === 200) {
+//                 console.log('purchase_seat successfully executed!');
+//             }
+//             }).catch(
+
+//                 this.props.history.push('/'),
+//             console.log('Something went wrong in purchase_seat API for api input data: ', data)
+//             )
+//     }
+    
+//     render() {
+//         let seatArr = [];
+//         for (let i = 1; i <= 30; i++) {
+//             if (this.state.seatTypeSelected == 'window') {
+//                 seatArr.push(i + "A");
+//             } else if (this.state.seatTypeSelected == 'middle') {
+//                 seatArr.push(i + "B");
+//             } if (this.state.seatTypeSelected == 'aisle') {
+//                 seatArr.push(i + "C");
+//             }
+//         }
+
+//         console.log("Abs",seatArr)
+
+//         let dropdown = (
+//             seatArr.map((num) => {
+//                 return (
+//                     <option value={num}>{num}</option>
+//                 );
+//             })
+//         );
+        
+//         console.log('$$$$$$$ ', this.state.seatTypeSelected, '   ', this.state.seatNumberSelected);
+
+//         return (
 //             <div className="purchase-seats-box">
-//                     <h1 style={{fontWeight: "bold"}}>Purchase Seats</h1>
-//             </div>
-
-//             <div id="seat-type-box" className="seat-selection">
-//                 <h3>Seat Price</h3>
-            
-//                 <ul>
-//                     {
-//                         Object.entries(seatPriceData)
-//                         .map( ([key, value]) => <li>{key}: $ {value}</li> )
-//                     }
-                    
-//                 </ul>
-//             </div>
-            
-//             <div id="seat-type-box" className="seat-selection">
-//                 <h3>Seat Type</h3>
-            
-//                 {/* <select id="seatType" value={selectedSeatType} onChange={(e)=>{handleSeatTypeSelection(e);}} */}
-//                 <select id="seatType" value={selectedSeatType} onChange={(e)=>{setSelectedSeatType(e.target.value)}}
-//                     style={{ width: "280px",height:"38px",border:"1px solid black" }}>
-
-//                     <option value="" disabled selected hidden>Select Seat Type</option>
-//                     {seatTypeData.map((seatType)=>{
-//                         return <option value={seatType}>{seatType}</option>
-//                 })}
-//                 </select>
-//             </div>
-
-//             <div id="seat-type-box" className="seat-selection">
-//                 <h3>Seat Number</h3>
-            
-//                 <select id="seatNum" value={selectedSeatNum} onChange={(e)=>{purchaseSeat(e);}}
-//                     style={{ width: "280px",height:"38px",border:"1px solid black" }}>
-
-//                     <option value="" disabled selected hidden>Select Seat Number</option>
-//                     {seatNumDictData[selectedSeatType].map((seatNum)=>{
-//                         return <option value={seatNum}>{seatNum}</option>
-//                 })}
-//                 </select>
-//             </div>
-
-//         </div>
-        
-//     )
-
-
-// }
-
-
-
-class PurchaseSeats extends Component {
-    
-    constructor(props) {
-        super();
-
-        var seatTypeData = [];
-        var seatPriceData = {};
-        var seatNumberData = [];
-
-        const url ="http://localhost:5000/booking/purchase_seat/61a276afea9044c96dd4c200";
-        Axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
-        Axios.get(url)
-        .then((response)=>{
-            console.log(response.data);
-            for (const [key, value] of Object.entries(response.data['num_of_seats'])) {
-                if (value > 0){
-                    seatTypeData.push(key)
-                }
-            }
-            console.log(seatTypeData)
-            seatPriceData = response.data['price']
-            seatNumberData = response.data['seat_num']
-
-        }).catch(()=>{
-            console.log('some error occurred!')
-        })
-
-        this.state = {
-            seatTypeData: [],
-            
-            booking_id: '',
-            seatTypeSelected: '',
-            seatNumberSelected: '',
-        }
-
-        this.seatTypeSelectHandler = this.seatTypeSelectHandler.bind(this);
-        this.seatNumberSelectHandler = this.seatNumberSelectHandler.bind(this);
-        this.seatSelectSubmission = this.seatSelectSubmission.bind(this);
-    }
-
-    
-
-    componentDidMount = () => {
-        this.setState({
-            seatTypeSelected: document.getElementById('seat-type-selection').value,
-            // seatNumberSelected: document.getElementById('seat-number-selection').value
-        });
-    }
-
-    seatTypeSelectHandler = (e) => {
-        console.log('DROPDOWN VALUE: ', e.target.value);
-        this.setState({
-            seatTypeSelected: e.target.value
-        });
-        if (e.target.value == 'window') {
-            this.setState({
-                seatNumberSelected: "1A"
-            });
-        } else if (e.target.value == 'middle') {
-            this.setState({
-                seatNumberSelected: "1B"
-            });
-        } if (e.target.value == 'aisle') {
-            this.setState({
-                seatNumberSelected: "1C"
-            });
-        }
-    }
-
-    seatNumberSelectHandler = (e) => {
-        console.log('DROPDOWN VALUE for seat number: ', e.target.value);
-        this.setState({
-            seatNumberSelected: e.target.value
-        });
-    }
-
-    seatSelectSubmission = (e) => {
-        console.log('Seat data finalized: ', this.state);
-
-        let data = {
-            booking_id: this.state.booking_id,
-            seat_type: this.state.seatTypeSelected,
-            // seat_number: this.state.seatNumberSelected
-        }
-
-        Axios.get('http://localhost:5000/booking/purchase_seat', data).
-            then((response)=>{
-            console.log('User get api response: ', response)
-            if (response.status === 200) {
-                console.log('purchase_seat successfully executed!');
-            }
-            }).catch(
-
-                this.props.history.push('/'),
-            console.log('Something went wrong in purchase_seat API for api input data: ', data)
-            )
-    }
-    
-    render() {
-        let seatArr = [];
-        for (let i = 1; i <= 30; i++) {
-            if (this.state.seatTypeSelected == 'window') {
-                seatArr.push(i + "A");
-            } else if (this.state.seatTypeSelected == 'middle') {
-                seatArr.push(i + "B");
-            } if (this.state.seatTypeSelected == 'aisle') {
-                seatArr.push(i + "C");
-            }
-        }
-
-        console.log("Abs",seatArr)
-
-        let dropdown = (
-            seatArr.map((num) => {
-                return (
-                    <option value={num}>{num}</option>
-                );
-            })
-        );
-        
-        console.log('$$$$$$$ ', this.state.seatTypeSelected, '   ', this.state.seatNumberSelected);
-
-        return (
-            <div className="purchase-seats-box">
-                <h1 style={{fontWeight: "bold"}}>Purchase Seats</h1>
+//                 <h1 style={{fontWeight: "bold"}}>Purchase Seats</h1>
 
                 
 
-                <div id="seat-type-box" className="seat-selection">
-                    <h3>Seat Type</h3>
+//                 <div id="seat-type-box" className="seat-selection">
+//                     <h3>Seat Type</h3>
 
-                    <select onChange={this.seatTypeSelectHandler} name="seat-type-selection" id="seat-type-selection">
-                        <option value="window">Window</option>
-                        <option value="middle">Middle</option>
-                        <option value="aisle">Aisle</option>
-                    </select>
-                </div>
+//                     <select onChange={this.seatTypeSelectHandler} name="seat-type-selection" id="seat-type-selection">
+//                         <option value="window">Window</option>
+//                         <option value="middle">Middle</option>
+//                         <option value="aisle">Aisle</option>
+//                     </select>
+//                 </div>
 
-                <div id="seat-number-box" className="seat-selection">
-                    <h3>Seat Number</h3>
-                    <select onChange={this.seatNumberSelectHandler} name="seat-number-selection" id="seat-number-selection">
-                        {dropdown}
-                    </select>
-                </div>
+//                 <div id="seat-number-box" className="seat-selection">
+//                     <h3>Seat Number</h3>
+//                     <select onChange={this.seatNumberSelectHandler} name="seat-number-selection" id="seat-number-selection">
+//                         {dropdown}
+//                     </select>
+//                 </div>
 
-                <button onClick={this.seatSelectSubmission} className="purchase-btns">Puchase Now</button>
-                <button className="purchase-btns">Purchase Later</button>
+//                 <button onClick={this.seatSelectSubmission} className="purchase-btns">Puchase Now</button>
+//                 <button className="purchase-btns">Purchase Later</button>
 
 
-            </div>
-        );
-    }
-}
+//             </div>
+//         );
+//     }
+// }
 
 export default PurchaseSeats;
