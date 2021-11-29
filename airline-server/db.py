8c7151f59de18c152bc01f3c212c2f84a688ed42
sai-kapadekar <91869107+sai-kapadekar@users.noreply.github.com>
2021-11-29 13:21:34 -0800

    Delete db.py

--- airline-server/db.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from __init__ import app
-CONNECTION_STRING = "mongodb+srv://SaiK:Mongo01%21@clustertechies.sdi1q.mongodb.net/techiesDb?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('techiesDB')
