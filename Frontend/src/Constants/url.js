9efdf962b390c45e3333613351ef6d83fbe5eef4
nidhi-tholar <nidhi.tholarkuchur@sjsu.edu>
2021-12-02 14:24:30 -0800

    Cloud instance backend url

--- Frontend/src/Constants/url.js
+++ Frontend/src/Constants/url.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "http://3.101.104.144:5000";
-export const baseUrl = " http://127.0.0.1:5000/";
+export const baseUrl = "http://54.176.8.82:5000";
+//export const baseUrl = " http://127.0.0.1:5000/";
