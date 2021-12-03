1c71c35ef63f8aaf2361660d46fdbe846f4e55ee
DESKTOP-MUBI1GV <sameerjoshi42@gmail.com>
2021-12-02 23:12:21 -0800

    delete mock data

--- Frontend/src/Components/Employee_Landing.js
+++ Frontend/src/Components/Employee_Landing.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import '../App.css'
 
 import { Navbar,Nav} from 'react-bootstrap';
-import {useState } from "react";
+
 const Employee_Landing = () => {
 
 
