4e871dad3fba3d24a4e7c0dc50e3a4c579e2196a
DESKTOP-MUBI1GV <sameerjoshi42@gmail.com>
2021-12-02 17:50:51 -0800

    add final

--- Frontend/src/Constants/url.js
+++ Frontend/src/Constants/url.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://18.117.77.18:5000";
-// export const baseUrl = " http://127.0.0.1:5000/";
+// export const baseUrl = "http://18.117.77.18:5000";
+export const baseUrl = " http://127.0.0.1:5000/";
